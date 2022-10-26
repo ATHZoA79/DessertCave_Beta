f0ac2b0e76ba39f3fbc4f7189255261e559e201e
ATHZoA79 <106025017anthonyhsu@gmail.com>
2022-10-26 11:10:43 +0800

    10261109
    - 完成header滾動 JS 動畫

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const body = document.querySelector('body');
+const main = document.querySelector('main');
+const header = document.querySelector('header');
+const LOGO = document.querySelector('#nav-logo');
+const PAGI = document.querySelector('.pagination>.content');
+const BANNER = document.querySelector('#banner');
+const SHOWCASE = document.querySelector('#showcase');
+
+var currentTop = 0;
+
+window.addEventListener('scroll', () => {
+  console.log(body.scrollTop, currentTop, window.scrollY);
+  if (window.scrollY < 830) {
+    header.classList.remove('header-hide');
+    header.classList.remove('header-show');
+    LOGO.classList.remove('logo-hide');
+    PAGI.classList.remove('pagination-span');
+  }else if (window.scrollY > currentTop) {
+    header.classList.remove('header-show');
+    header.classList.add('header-hide');
+    LOGO.classList.add('logo-hide');
+    PAGI.classList.add('pagination-span');
+  }else if (window.scrollY < currentTop) {
+    header.classList.remove('header-hide');
+    header.classList.add('header-show');
+    LOGO.classList.remove('logo-hide');
+    PAGI.classList.add('pagination-span');
+  }
+  currentTop = window.scrollY;
+});
