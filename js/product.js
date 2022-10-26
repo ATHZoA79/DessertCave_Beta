d6e7296824abd8001ecfa89dca7ca8c9e8873989
Tyyy2 <uasdfggt890131@gmail.com>
2022-10-27 05:06:24 +0800

    完成

--- js/product.js
+++ js/product.js
@@ -29,17 +29,21 @@ var product_detail_content = [
 
 
 
-//--first product detail
-var product_card = document.querySelectorAll('.product-card');
-product_card.forEach(card => {
-    card.addEventListener('click', function show_detail() {
-        product_detail.classList.toggle('block-display');
-        product_detail.innerHTML = `
+
+
+
+if (screen.width < 430) {
+    //--first product detail
+    var product_card = document.querySelectorAll('.product-card');
+    product_card.forEach(card => {
+        card.addEventListener('click', function show_detail() {
+            product_detail.classList.toggle('block-display');
+            product_detail.innerHTML = `
                     <div class="product-detail-opacity"></div>
                     <div class="product-detail-card-container">
                       <div class="product-detail-card">
                         <div class="product-detail-close"><i class="bi bi-x-lg"></i></div>
-                        <div class="product-detail-img"><img src="./img/product/product_list/detail/${card.querySelector('.img-container>img').dataset.product}-detail.png" alt=""></div>
+                        <div class="product-detail-img"><img src="./img/product/product_list/detail/${card.querySelector('.img-container>img').dataset.product}-detail-phone.png" alt=""></div>
                         <div class="product-detail-text">
                           <div class="product-detail-name">${card.querySelector('.product-name>h3').innerText}</div>
                           <div class="product-detail-content">${product_detail_content[0][`${card.querySelector('.img-container>img').dataset.product}`]}</div>
@@ -48,23 +52,21 @@ product_card.forEach(card => {
                             target="_blank">了解更多...</a></div>
                       </div>
                     ` ;
-        var product_detail_close = document.querySelector('.product-detail-close');
-        var product_detail_opacity = document.querySelector('.product-detail-opacity');
+            var product_detail_close = document.querySelector('.product-detail-close');
+            var product_detail_opacity = document.querySelector('.product-detail-opacity');
 
-        product_detail_close.addEventListener('click', close_detail);
-        product_detail_opacity.addEventListener('click', close_detail);
-
-
-    })
-
-});
+            product_detail_close.addEventListener('click', close_detail);
+            product_detail_opacity.addEventListener('click', close_detail);
 
 
+        })
 
-//--End of first product detail
+    });
 
 
-if (screen.width < 430) {
+
+    //--End of first product detail
+
 
     //--product detail
     product_sheet.forEach(sheet => {
@@ -560,7 +562,40 @@ if (screen.width < 430) {
 
 }
 else {
-    console.log('真的大於430');
+    //--first product detail
+    var product_card = document.querySelectorAll('.product-card');
+    product_card.forEach(card => {
+        card.addEventListener('click', function show_detail() {
+            product_detail.classList.toggle('block-display');
+            product_detail.innerHTML = `
+                    <div class="product-detail-opacity"></div>
+                    <div class="product-detail-card-container">
+                      <div class="product-detail-card">
+                        <div class="product-detail-close"><i class="bi bi-x-lg"></i></div>
+                        <div class="product-detail-img"><img src="./img/product/product_list/detail/${card.querySelector('.img-container>img').dataset.product}-detail.png" alt=""></div>
+                        <div class="product-detail-text">
+                          <div class="product-detail-name">${card.querySelector('.product-name>h3').innerText}</div>
+                          <div class="product-detail-content">${product_detail_content[0][`${card.querySelector('.img-container>img').dataset.product}`]}</div>
+                        </div>
+                        <div class="product-detail-button"><a href="https://zh-tw.facebook.com/dessertcave0725/"
+                            target="_blank">了解更多...</a></div>
+                      </div>
+                    ` ;
+            var product_detail_close = document.querySelector('.product-detail-close');
+            var product_detail_opacity = document.querySelector('.product-detail-opacity');
+
+            product_detail_close.addEventListener('click', close_detail);
+            product_detail_opacity.addEventListener('click', close_detail);
+
+
+        })
+
+    });
+
+
+
+    //--End of first product detail
+
     //--product detail
     product_sheet.forEach(sheet => {
         sheet.addEventListener('click', function () {
